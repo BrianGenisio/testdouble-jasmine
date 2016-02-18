2afc662b4b5206ea30863ce4a3155b176b962025
Brian Genisio <BrianGenisio@Gmail.com>
2016-02-18 16:55:18 -0500

    Getting it working for 1.x style matcher

--- test/testdouble-jasmine_spec.js
+++ test/testdouble-jasmine_spec.js
@@ -1,12 +1,29 @@
 var td = require('testdouble');
 var tdJasmine = require('../lib/testdouble-jasmine');
 
-tdJasmine(td, jasmine);
+tdJasmine(td);
 
 describe("testdouble-jasmine", function() {
 
-  it('passes', () => {
-    expect(true).toBe(true);
+  beforeEach(function() {
+    this.subject = td.function("dubs");
+  });
+
+  describe('toVerify', function() {
+    it('verifies if the given testdouble was called', function() {
+      this.subject();
+      expect().toVerify(this.subject());
+    });
+
+    it('doesnt verify if the testdouble was not called', function() {
+      expect().not.toVerify(this.subject());
+    });
+
+    it('doesnt verify if called on an object that isnt a testdouble', function() {
+      var not_a_testdouble = function() {};
+      not_a_testdouble();
+      expect().not.toVerify(not_a_testdouble());
+    });
   });
 
 });
